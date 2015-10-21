d52b6be8ff4f9305e5482891f6098af860a2821f
gsantella <gsantella@southhills.edu>
2015-10-20 23:19:57 -0400

    add multiple pages

--- ProgramInterestSurveyApp/js/app.js
+++ ProgramInterestSurveyApp/js/app.js
@@ -77,6 +77,168 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
             }
         })
 
+        .state('page4', {
+                url:'/page4',
+                views: {
+                  'header': {
+                    templateUrl:'partials/header.html',
+                    controller: 'HomeCtrl'
+                  },
+                  'content': {
+                    templateUrl:'partials/page4.html',
+                    controller: 'HomeCtrl'
+                  },
+                  'footer': {
+                    templateUrl:'partials/footer.html',
+                    controller: 'HomeCtrl'
+                  },
+                }
+        })
+
+        .state('page5', {
+            url:'/page5',
+            views: {
+              'header': {
+                templateUrl:'partials/header.html',
+                controller: 'HomeCtrl'
+              },
+              'content': {
+                templateUrl:'partials/page5.html',
+                controller: 'HomeCtrl'
+              },
+              'footer': {
+                templateUrl:'partials/footer.html',
+                controller: 'HomeCtrl'
+              },
+            }
+          })
+
+          .state('page6', {
+              url:'/page6',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page6.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('page7', {
+              url:'/page7',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page7.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('page8', {
+              url:'/page8',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page8.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('page9', {
+              url:'/page9',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page9.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('page10', {
+              url:'/page10',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page10.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('page11', {
+              url:'/page11',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/page11.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
+          .state('results', {
+              url:'/results',
+              views: {
+                'header': {
+                  templateUrl:'partials/header.html',
+                  controller: 'HomeCtrl'
+                },
+                'content': {
+                  templateUrl:'partials/results.html',
+                  controller: 'HomeCtrl'
+                },
+                'footer': {
+                  templateUrl:'partials/footer.html',
+                  controller: 'HomeCtrl'
+                },
+              }
+          })
+
 }]);
 
 app.controller('HomeCtrl',  ['$scope', function($scope) {
@@ -100,11 +262,11 @@ app.controller('HomeCtrl',  ['$scope', function($scope) {
   }
 
   $scope.programs = [
-    {text:'Program1', value:$scope.countTrues($scope.checkboxModel)},
+    {text:'Program1', value:$scope.countTrues($scope.checkboxModelPage1)},
     {text:'Program2', value:$scope.countTrues($scope.checkboxModelPage2)}
   ];
 
-  $scope.checkboxModel = $scope.checkboxModel || {
+  $scope.checkboxModelPage1 = $scope.checkboxModelPage1 || {
        value1 : false, value2 : false, value3 : false, value4 : false,
        value5 : false, value6 : false, value7 : false, value8 : false,
        value9 : false, value10: false, value11 : false, value12 : false,
@@ -113,6 +275,78 @@ app.controller('HomeCtrl',  ['$scope', function($scope) {
   };
 
   $scope.checkboxModelPage2 = $scope.checkboxModelPage2 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage3 = $scope.checkboxModelPage3 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage3 = $scope.checkboxModelPage4 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage4 = $scope.checkboxModelPage5 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage5 = $scope.checkboxModelPage6 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage6 = $scope.checkboxModelPage7 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage7 = $scope.checkboxModelPage8 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage8 = $scope.checkboxModelPage9 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage9 = $scope.checkboxModelPage10 || {
+       value1 : false, value2 : false, value3 : false, value4 : false,
+       value5 : false, value6 : false, value7 : false, value8 : false,
+       value9 : false, value10: false, value11 : false, value12 : false,
+       value13 : false, value14 : false, value15 : false, value16 : false,
+       value17 : false
+  };
+
+  $scope.checkboxModelPage10 = $scope.checkboxModelPage11 || {
        value1 : false, value2 : false, value3 : false, value4 : false,
        value5 : false, value6 : false, value7 : false, value8 : false,
        value9 : false, value10: false, value11 : false, value12 : false,
